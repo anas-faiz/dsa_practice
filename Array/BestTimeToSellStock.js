a6112180f8abe72dfc0a2764d4c2800158f9a18f
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-05 11:00:49 -0700

    best time to sell stocks

---
+++ Array/BestTimeToSellStock.js
@@ -0,0 +1,19 @@
+//problem : search and find in an array 
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1760718679/
+
+var maxProfit = function(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let price of prices) {
+        // Update the minimum price so far
+        if (price < minPrice) {
+            minPrice = price;
+        } else {
+            // Calculate profit if we sold at current price
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+    }
+
+    return maxProfit;
+};
