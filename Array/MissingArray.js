c96e7cb0ad0d15c682a2407807274770d683de19
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-10 06:38:51 -0700

    missing integer in an array(GFG)

---
+++ Array/MissingArray.js
@@ -0,0 +1,23 @@
+// problem : Missing integer in an array
+// https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1?page=1&category=Arrays&sortBy=submissions
+
+//solution
+
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+class Solution {
+    missingNum(arr) {
+        let n = arr.length + 1;
+        let total = (n * (n + 1)) / 2;
+
+        let sum1 = 0;
+        for (let i = 0; i < arr.length; i++) {
+            sum1 += arr[i];
+        }
+
+        return total - sum1;
+    }
+}
+
