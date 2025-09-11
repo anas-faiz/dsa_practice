dc14c1bbe355d38efbfa032997f30a10bbce5bd0
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-11 07:14:45 -0700

    binary search questions

--- Search&sorting/BinarySearch.js
+++ Search&sorting/BinarySearch.js
@@ -1,17 +1,20 @@
 //problem : Search X in sorted array
-//link : https://takeuforward.org/plus/dsa/problems/search-x-in-sorted-array
-// time complexity : 
+//link : https://leetcode.com/problems/binary-search/submissions/1767218309/
+// time complexity : O(log n)
 
 //soluiton
-class Solution {
-    search(nums, target) {
-       let low = 0 ; let high = nums.length - 1;let mid;
-       while(low<=high){
-        mid = Math.floor((low + high)/2);
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let mid ; let low = 0 ; let high = nums.length -1;
+    while(low <= high){
+        mid =Math.floor(((low+high)/2));
         if(nums[mid] == target) return mid;
-        else if (nums[mid] < target ) low = mid + 1;
-        else high = mid - 1  ;
-       }
-       return -1
-    }
-}
+        else if (nums[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    } 
+    return - 1
+};
---
+++ Search&sorting/SearchInsertPosition.js
@@ -0,0 +1,20 @@
+// problem : search insert position if element is not found in an sorted array 
+// link : https://leetcode.com/problems/search-insert-position/submissions/
+// time complxity : O(logN)
+//solution :
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let mid ; let low = 0 ;let high = nums.length - 1;
+    while (low <= high ){
+        mid = Math.floor((low + high )/2)
+        if (nums[mid] == target) return mid ;
+        else if (nums[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return low;
+};
