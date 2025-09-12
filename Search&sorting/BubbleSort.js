aca43cd82dbb53417af3299f2cf9af528f439df1
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-12 12:17:29 -0700

    bubble sort

---
+++ Search&sorting/BubbleSort.js
@@ -0,0 +1,28 @@
+// Bubble sort
+// link : https://www.geeksforgeeks.org/problems/bubble-sort/1
+// time complxity : O(n*n) worst case /O(n) best case
+//solution
+/**
+ * @param {number[]} arr
+ */
+class Solution {
+    bubbleSort(arr) {
+        // code here
+        let n= arr.length;
+        for (let i = n-1 ;i >=0; i--){
+            let didSwap = 0;
+            for (let j = 0; j<=i;j++){
+                if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                 arr[j+1] = temp;
+                didSwap=1;
+                    
+                }
+            }
+            if(didSwap == 0){
+                break;
+            }
+        }
+    }
+}
