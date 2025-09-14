897cc591a05730aaaa78cf3744a048e217439b4c
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-14 12:40:51 -0700

    better solution can be done

---
+++ Strings/FindAnCharacter.js
@@ -0,0 +1,24 @@
+//problem: Find Words containing character
+//link: https://leetcode.com/problems/find-words-containing-character/submissions/1770881329/
+//time complxity : O(n 2)
+//solution:
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+  let n = words.length;
+  let arr = [];
+  for (let i = 0 ; i<n ; i++){
+    let s = words[i];
+    let m = s.length
+    for(let j = 0 ; j < m ; j++ ){
+        if(s[j] == x){
+          arr.push(i);
+          break;            
+        }
+    }
+    }
+    return arr;  
+};
