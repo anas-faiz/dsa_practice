57e2c844cdc0628e18de2e2334cdb24b2f40f37b
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-16 10:49:37 -0700

    Balanced String

---
+++ Strings/BalancedString.js
@@ -0,0 +1,23 @@
+//problem : split a string in a blanced string 
+//Link : https://leetcode.com/problems/split-a-string-in-balanced-strings/submissions/1773055552/
+// time complexity : O(n)
+//solution 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let balance = 0;
+    let count = 0;
+    
+    for (let ch of s) {
+        if (ch === 'L') balance++;
+        else balance--;
+        
+        if (balance === 0) {
+            count++;
+        }
+    }
+    
+    return count;
+};
