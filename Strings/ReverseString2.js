ccaaa0602f2489d6942d7de5db9c1dc82049a028
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-20 11:33:34 -0700

    Reverse a string*

---
+++ Strings/ReverseString2.js
@@ -0,0 +1,22 @@
+//problem : Reverse a string
+//link : https://leetcode.com/problems/reverse-string-ii/submissions/1777310809/
+
+//solution:
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+    let arr = s.split("");
+    for (let i = 0; i < arr.length; i += 2 * k) {
+        let left = i;
+        let right = Math.min(i + k - 1, arr.length - 1);
+        while (left < right) {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+    return arr.join("");
+};
