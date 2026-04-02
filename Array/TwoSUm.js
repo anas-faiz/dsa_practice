dd0703aee80a1d3dff6a902f5826d9c2818a2a86
anas-faiz <anasfaiz0811@gmail.com>
2026-04-01 18:39:27 -0700

    two sum test

--- Array/TwoSUm.js
+++ Array/TwoSUm.js
@@ -1,6 +1,4 @@
-
 // My SOlution
-
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -19,6 +17,26 @@ var twoSum = function(nums, target) {
                 
             }
         }
+};
+//my optimal solution
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+        const length = nums.length;
+        if(length == 0) return ;
+        let seen = new Map();
+        for(let i = 0; i<length;i++){
+            let req = target - nums[i];
+            if(seen.has(req)){
+                return [seen.get(req),i]
+            }
+            seen.set(nums[i],i)
+            
+        }
+        return [];
 };
 
 //Optimal solution
